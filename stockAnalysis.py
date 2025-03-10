69f9be19cf285b77118a5462bfbf305e21097fd7
bansalshubham257 <bansalshubham257@gmail.com>
2025-03-11 00:06:54 +0530

    Update stockAnalysis.py

--- stockAnalysis.py
+++ stockAnalysis.py
@@ -6,6 +6,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import ssl
 import yfinance as yf
+import os
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": ["https://swingtradingwithme.blogspot.com"]}})
