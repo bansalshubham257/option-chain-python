73cafbe118f49898d06c7820150a50e7704bcd32
bansalshubham257 <bansalshubham257@gmail.com>
2025-03-27 10:24:19 +0530

    Update ws_service.py

--- ws_service.py
+++ ws_service.py
@@ -66,4 +66,5 @@ def get_indices_data():
         }), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
