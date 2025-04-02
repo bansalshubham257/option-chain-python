e238701b05dce0266968d01780202d66152ff1ad
bansalshubham257 <bansalshubham257@gmail.com>
2025-04-02 16:43:06 +0530

    Update test.py
    added cors

--- test.py
+++ test.py
@@ -28,7 +28,7 @@ from config import ACCESS_TOKEN  # Store securely
 
 app = Flask(__name__)
 
-CORS(app, resources={r"/*": {"origins": ["https://swingtradingwithme.blogspot.com", "https://aitradinglab.blogspot.com", "https://bansalshubham257.github.io"]}})
+CORS(app, resources={r"/*": {"origins": ["https://swingtradingwithme.blogspot.com", "https://aitradinglab.blogspot.com", "https://www.aitradinglab.in/", "https://bansalshubham257.github.io"]}})
 ssl._create_default_https_context = ssl._create_unverified_context     
 
 # Database connection setup
