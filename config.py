13ebabb33f9d1945d8c18efa0229193971b23bff
bansalshubham257 <bansalshubham257@gmail.com>
2025-04-24 07:29:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config:
     ACCESS_TOKEN = os.getenv('ACCESS_TOKEN', 'eyJ0eXAiOiJKV1QiLCJrZXlfaWQiOiJza192MS4wIiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiIyWEJSUFMiLCJqdGkiOiI2N2VkZjI5OTVlMDFkYTVlZjBjY2Q5ODAiLCJpc011bHRpQ2xpZW50IjpmYWxzZSwiaWF0IjoxNzQzNjQ3Mzg1LCJpc3MiOiJ1ZGFwaS1nYXRld2F5LXNlcnZpY2UiLCJleHAiOjE3NDM3MTc2MDB9.Ra7Bclq3ysxWNmi7oJol_1mcgz1sCK7WWgFG-59ZFmM')
 
     # Other configurations
-    EXPIRY_DATE = "2025-04-24"
+    EXPIRY_DATE = "2025-05-29"
     # Market hours configuration
     MARKET_OPEN = time(9, 15)  # 09:15 AM
     MARKET_CLOSE = time(15, 30)  # 03:30 PM
