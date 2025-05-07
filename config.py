2d98209298b6bdf9ec085916d0ae57d7818475b7
bansalshubham257 <bansalshubham257@gmail.com>
2025-05-08 00:07:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,6 +15,10 @@ class Config:
     MARKET_OPEN = time(9, 12)  # 09:15 AM
     MARKET_CLOSE = time(15, 30)  # 03:30 PM
     
+    # Post-market window for financial data collection (3:35 PM - 3:39 PM)
+    POST_MARKET_START = time(15, 35)
+    POST_MARKET_END = time(15, 39)
+
     # Database clearing time window
     DB_CLEARING_START = time(9, 0)  # 09:00 AM
     DB_CLEARING_END = time(9, 15)  # 09:15 AM
@@ -36,22 +40,4 @@ class Config:
         "2025-05-15",
         "2025-05-22",
         "2025-05-29"
-    ]
-
-    # Data collection times with buffer windows (1-3 minutes before and after target times)
-    # This ensures cron jobs can trigger within a small window around the desired times
-    COMBINED_DATA_COLLECTION_TIMES = [
-        # 10:00 AM buffer window (9:57 AM to 10:03 AM)
-        
-        time(10, 00),
-        
-        # 12:00 PM buffer window (11:57 AM to 12:03 PM)
-        
-        time(11, 50),
-
-        time(14, 25),
-        # 3:00 PM buffer window (2:57 PM to 3:03 PM)
-        
-        time(15, 25)
-        
     ]
