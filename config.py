ff245c6dc875c1278722d5a7a18b7bbc4e220aac
bansalshubham257 <bansalshubham257@gmail.com>
2025-06-25 21:08:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,10 +13,10 @@ class Config:
     ACCESS_TOKEN2 = os.getenv('ACCESS_TOKEN2', 'eyJ0eXAiOiJKV1QiLCJrZXlfaWQiOiJza192MS4wIiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiIyWEJSUFMiLCJqdGkiOiI2N2VkZjI5OTVlMDFkYTVlZjBjY2Q5ODAiLCJpc011bHRpQ2xpZW50IjpmYWxzZSwiaWF0IjoxNzQzNjQ3Mzg1LCJpc3MiOiJ1ZGFwaS1nYXRld2F5LXNlcnZpY2UiLCJleHAiOjE3NDM3MTc2MDB9.Ra7Bclq3ysxWNmi7oJol_1mcgz1sCK7WWgFG-59ZFmM')
 
     # Other configurations
-    EXPIRY_DATE = "2025-06-26"
+    EXPIRY_DATE = "2025-07-31"
     # Market hours configuration
     MARKET_OPEN = time(9, 0)  # 09:15 AM
-    MARKET_CLOSE = time(15, 33)  # 03:30 PM
+    MARKET_CLOSE = time(23, 33)  # 03:30 PM
     
     # Post-market window for financial data collection (3:35 PM - 3:39 PM)
     POST_MARKET_START = time(15, 35)
@@ -29,18 +29,18 @@ class Config:
     TRADING_DAYS = {0, 1, 2, 3, 4}  # Monday to Friday
 
     SENSEX_EXPIRIES = [
-        "2025-06-24"
+        "2025-07-01"
     ]
 
     BANKEX_EXPIRIES = [
-        "2025-06-24"
+        "2025-07-29"  # Fixed typo: 067-29 -> 07-29
     ]
 
     NIFTY_EXPIRIES = [
-        "2025-06-26"
+        "2025-07-03"
     ]
 
     # These should include all relevant expiry dates for different instruments
     INSTRUMENT_EXPIRIES = [
-        "2025-06-26"
+        "2025-07-31"
     ]
