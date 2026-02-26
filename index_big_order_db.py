1bd9f9c7e2ccb8acead39d0b0489f0e24c51cd90
bansalshubham257 <bansalshubham257@gmail.com>
2026-02-26 17:04:37 +0530

    Update index_big_order_db.py

--- index_big_order_db.py
+++ index_big_order_db.py
@@ -25,7 +25,7 @@ CURRENT_TOKEN_INDEX = 0  # Track which token to use
 
 # 2. ENTER EXPIRY DATES (Format: YYYY-MM-DD)
 NIFTY_EXPIRY      = "2026-03-02"   # NIFTY options expiry
-SENSEX_EXPIRY     = "2026-02-26"   # SENSEX options expiry (BSE_FO)
+SENSEX_EXPIRY     = "2026-03-05"   # SENSEX options expiry (BSE_FO)
 STOCK_FNO_EXPIRY  = "2026-03-30"   # NSE stock options expiry (OPTSTK)
 CRUDE_EXPIRY      = "2029-12-16"   # MCX Crudeoil expiry
 NG_EXPIRY         = "2029-12-23"   # MCX Natural Gas expiry
